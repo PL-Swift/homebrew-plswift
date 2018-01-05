7de12f4a0decfbc43756ecdd86e6811b64f24b4a
Helge Hess <devteam@zeezide.de>
2018-01-06 00:53:52 +0100

    Use 0.0.3

--- plswift.rb
+++ plswift.rb
@@ -2,9 +2,9 @@ class PLSwift < Formula
 
   desc "Bringing Swift to the Backend of the Backend's Backend!"
   homepage "https://github.com/PL-Swift"
-  url "https://github.com/PL-Swift/PLSwift/archive/0.0.2.tar.gz"
-  # curl -L https://github.com/PL-Swift/PLSwift/archive/0.0.2.tar.gz | shasum -a 256
-  sha256 "0d464eabfaf86d9cd5052d0f1b27aad522a8fec4dbabca51943401b44259dbf9"
+  url "https://github.com/PL-Swift/PLSwift/archive/0.0.3.tar.gz"
+  # curl -L https://github.com/PL-Swift/PLSwift/archive/0.0.3.tar.gz | shasum -a 256
+  sha256 "324a63b1feb234ccc8691b8ecfb644d550a4571eed6c188e295ad9b9dfaa59f5"
   
   depends_on "pkg-config"
 
